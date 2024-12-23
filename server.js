38b2e90319e9b525b74550b999c924aec9d86157
gaurav5xy <gauravkumar81464@gmail.com>
2024-12-23 19:17:06 +0530

    cors-allow

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors('*'));
+app.use(cors('http://localhost:5173/'));
 app.use(bodyParser.json());
 
 // MongoDB Connection
